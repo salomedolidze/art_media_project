24c52399697b4adfee3d202b418ff726341eb5d0
salome <111355602+salomedolidze@users.noreply.github.com>
2023-04-17 01:22:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,25 +8,3 @@ navigataionBar.classList.toggle('navbaractive')
 });
 
 
-
-$('#recipeCarousel').carousel({
-    interval: 10000
-  })
-  
-  $('.carousel .carousel-item').each(function(){
-      var minPerSlide = 3;
-      var next = $(this).next();
-      if (!next.length) {
-      next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
-      
-      for (var i=0;i<minPerSlide;i++) {
-          next=next.next();
-          if (!next.length) {
-              next = $(this).siblings(':first');
-            }
-          
-          next.children(':first-child').clone().appendTo($(this));
-        }
-  });
