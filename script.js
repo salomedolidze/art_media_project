fc6a42f41fbb2bba2c40b1601362b15acdd59584
salome <111355602+salomedolidze@users.noreply.github.com>
2023-04-17 01:18:52 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+///burger menu
+
+ let navigataionBar = document.querySelector('.header_ul');
+let navigationToggle = document.querySelector('.burger_menu');
+navigationToggle.addEventListener('click', function ()
+{
+navigataionBar.classList.toggle('navbaractive')
+});
+
+
+
+$('#recipeCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
